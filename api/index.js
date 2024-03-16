05b11f2689b6580c311e5d176fdce5426d5b5c0b
Fam <fam@Linhs-MacBook-Air.local>
2024-03-16 14:12:44 +0700

    server: connect db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log("Db err", err);
+  });
 
 const app = express();
 const PORT = 3006;
