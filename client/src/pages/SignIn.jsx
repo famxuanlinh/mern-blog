2e50536d752e948a33a8c9a0af7ca62eeead03a3
Fam <fam@Linhs-MacBook-Air.local>
2024-03-21 21:45:21 +0700

    feat: add redux

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,17 +1,46 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
-// import { useState } from 'react';
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import { useDispatch, useSelector } from 'react-redux';
-// import {
-//   signInStart,
-//   signInSuccess,
-//   signInFailure,
-// } from '../redux/user/userSlice';
-// import OAuth from '../components/OAuth';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
   const handleSubmit = async (e) => {
-    console.log("Singin sublit");
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure("Please fill all the fields"));
+    }
+    try {
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+      }
+
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
   };
   return (
     <div className="min-h-screen mt-20">
@@ -20,7 +49,7 @@ export default function SignIn() {
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Farm
+              Sahand's
             </span>
             Blog
           </Link>
@@ -39,7 +68,7 @@ export default function SignIn() {
                 type="email"
                 placeholder="name@company.com"
                 id="email"
-                onChange={() => {}}
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -48,22 +77,22 @@ export default function SignIn() {
                 type="password"
                 placeholder="**********"
                 id="password"
-                onChange={() => {}}
+                onChange={handleChange}
               />
             </div>
             <Button
               gradientDuoTone="purpleToPink"
               type="submit"
-              //   disabled={loading}
+              disabled={loading}
             >
-              {/* {loading ? (
+              {loading ? (
                 <>
-                  <Spinner size='sm' />
-                  <span className='pl-3'>Loading...</span>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
                 </>
-              ) : ( */}
-              Sign In
-              {/* )} */}
+              ) : (
+                "Sign In"
+              )}
             </Button>
             {/* <OAuth /> */}
           </form>
@@ -73,11 +102,11 @@ export default function SignIn() {
               Sign Up
             </Link>
           </div>
-          {/* {errorMessage && (
-            <Alert className='mt-5' color='failure'>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
               {errorMessage}
             </Alert>
-          )} */}
+          )}
         </div>
       </div>
     </div>
